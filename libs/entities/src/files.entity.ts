f3924bfa01331b434204f9c1cc6648439bfa1697
Niga9L <lyaginvanya@gmail.com>
2023-05-12 19:20:05 +0300

    LIB: add entities

---
+++ libs/entities/src/files.entity.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity('files')
+export class FilesEntity {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  link: string;
+
+  @Column({ name: 'user_id' })
+  userId: string;
+
+  @Column({ name: 'is_published' })
+  isPublished: string;
+
+  @Column({ name: 'created_at' })
+  createdAt: string;
+
+  @Column({ name: 'updated_at' })
+  updatedAt: string;
+
+  @Column({ name: 'deleted_at' })
+  deletedAt: string;
+}
